f7e68a3f6396104b1c1e156fd6122b85a6c2b324
Daniel Jesus <dev.danieljs@gmail.com>
2026-04-01 00:03:31 -0300

    chore(example): update player script exports

--- scripts/player.lua
+++ scripts/player.lua
@@ -1,3 +1,8 @@
+export = {
+    speed = 3.0,
+    rot_speed = 2.5
+}
+
 function init(id)
 end
 
@@ -5,7 +10,28 @@ function ready(id)
 end
 
 function update(id, dt)
-    engine.entity.move(id, 0.75 * dt, 0.0)
+    local speed = export.speed
+    if engine.input.is_key_down(340) then -- MLX_KEY_LEFT_SHIFT
+        speed = speed * 1.75
+    end
+    if engine.input.is_key_down(87) then -- MLX_KEY_W
+        engine.entity.move(id, speed * dt, 0.0)
+    end
+    if engine.input.is_key_down(83) then -- MLX_KEY_S
+        engine.entity.move(id, -speed * dt, 0.0)
+    end
+    if engine.input.is_key_down(65) then -- MLX_KEY_A
+        engine.entity.move(id, 0.0, -speed * dt)
+    end
+    if engine.input.is_key_down(68) then -- MLX_KEY_D
+        engine.entity.move(id, 0.0, speed * dt)
+    end
+    if engine.input.is_key_down(263) then -- MLX_KEY_LEFT
+        engine.entity.rotate(id, -export.rot_speed * dt)
+    end
+    if engine.input.is_key_down(262) then -- MLX_KEY_RIGHT
+        engine.entity.rotate(id, export.rot_speed * dt)
+    end
 end
 
 function destroy(id)
